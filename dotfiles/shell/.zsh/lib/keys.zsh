f7e32a8ded5ad96f0ee91c315c62500d99c8d319
graysky <graysky@archlinux.us>
2013-01-05 08:46:43 -0500

    reverted

--- dotfiles/shell/.zsh/lib/keys.zsh
+++ dotfiles/shell/.zsh/lib/keys.zsh
@@ -2,6 +2,9 @@ bindkey -v # set vim bindings	http://zshwiki.org/home/zle/bindkeys
 
 typeset -A key
 
+bindkey "^[[A" history-beginning-search-backward
+bindkey "^[[B" history-beginning-search-forward
+
 key[Home]=${terminfo[khome]}
 key[End]=${terminfo[kend]}
 key[Insert]=${terminfo[kich1]}
@@ -19,8 +22,6 @@ key[Enter]=${terminfo[kent]}
 [[ -n "${key[End]}"     ]]  && bindkey  "${key[End]}"     end-of-line
 [[ -n "${key[Insert]}"  ]]  && bindkey  "${key[Insert]}"  overwrite-mode
 [[ -n "${key[Delete]}"  ]]  && bindkey  "${key[Delete]}"  delete-char
-[[ -n "${key[Up]}"      ]]  && bindkey  "${key[Up]}"      history-beginning-search-backward
-[[ -n "${key[Down]}"    ]]  && bindkey  "${key[Down]}"    history-beginning-search-forward
 [[ -n "${key[Left]}"    ]]  && bindkey  "${key[Left]}"    backward-char
 [[ -n "${key[Right]}"   ]]  && bindkey  "${key[Right]}"   forward-char
 [[ -n "${key[Enter]}"   ]]  && bindkey  "${key[Enter]}"   enter
