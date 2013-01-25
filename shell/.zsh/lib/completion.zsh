f9ff7de4625cdfd83ab07a72b0ff4ed837bb6877
graysky <graysky@archlinux.us>
2013-01-25 10:28:57 -0500

    modifications to zsh configs

--- shell/.zsh/lib/completion.zsh
+++ shell/.zsh/lib/completion.zsh
@@ -25,7 +25,6 @@ cdpath=(.)
 
 [ -r /etc/ssh/ssh_known_hosts ] && _global_ssh_hosts=(${${${${(f)"$(</etc/ssh/ssh_known_hosts)"}:#[\|]*}%%\ *}%%,*}) || _ssh_hosts=()
 [ -r ~/.ssh/known_hosts ] && _ssh_hosts=(${${${${(f)"$(<$HOME/.ssh/known_hosts)"}:#[\|]*}%%\ *}%%,*}) || _ssh_hosts=()
-#[ -r /etc/hosts ] && : ${(A)_etc_hosts:=${(s: :)${(ps:\t:)${${(f)~~"$(</etc/hosts)"}%%\#*}##[:blank:]#[^[:blank:]]#}}} || _etc_hosts=()
 hosts=(
   "$_global_ssh_hosts[@]"
   "$_ssh_hosts[@]"
--- shell/.zsh/lib/correction.zsh
+++ shell/.zsh/lib/correction.zsh
@@ -1,11 +1,9 @@
 setopt correct_all
 
 alias man='nocorrect man'
+alias cp='nocorrect cp'
+alias vim='nocorrect vim'
 alias mv='nocorrect mv'
 alias mysql='nocorrect mysql'
 alias mkdir='nocorrect mkdir'
-alias gist='nocorrect gist'
-alias heroku='nocorrect heroku'
-alias ebuild='nocorrect ebuild'
-alias hpodder='nocorrect hpodder'
 alias sudo='nocorrect sudo'
